12cc87d0470c904bcccf042276d2110d36fbc369
zww <2521160370@qq.com>
2020-07-28 19:17:25 +0800

    将前端分页功能改为ajax版本

--- src/main/java/com/atguigu/crud/controller/EmployeeController.java
+++ src/main/java/com/atguigu/crud/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.atguigu.crud.controller;
 
 import com.atguigu.crud.bean.Employee;
+import com.atguigu.crud.bean.Msg;
 import com.atguigu.crud.service.EmployeeService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -27,6 +29,21 @@ public class EmployeeController {
      * @return
      */
     @RequestMapping("/emps")
+    @ResponseBody
+    public Msg getEmpsWithJson(@RequestParam(value = "pn",defaultValue = "1") Integer pn){
+        PageHelper.startPage(pn,5);
+        //startPage后的查询就是分页查询
+        List<Employee> emps = employeeService.getAll();
+
+        //使用pageinfo包装查询后的结果
+        //封装了详细的分页信息
+        PageInfo pageInfo = new PageInfo(emps,5);//连续显示5页
+        return Msg.success().add("pageInfo",pageInfo);
+
+    }
+
+
+    //@RequestMapping("/emps")
     public String getEmps(@RequestParam(value = "pn",defaultValue = "1") Integer pn, Model model){
         //引入PageHelper插件
         //在查询之前调用
